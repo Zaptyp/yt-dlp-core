7c652bab3c15511634221e1b081a2ac8c4f91872
Patryk <43276401+Zaptyp@users.noreply.github.com>
2025-03-16 08:44:41 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -281,11 +281,12 @@ export default class YTDlpWrap {
         let stderrData = '';
         let processError: Error;
         ytDlpProcess.stdout.on('data', (data) =>
-            YTDlpWrap.emitYoutubeDlEvents(data.toString(), execEventEmitter)
+            YTDlpWrap.emitYoutubeDlEvents(data.toString(), execEventEmitter),
+            console.log(data.toString())
         );
         ytDlpProcess.stderr.on(
             'data',
-            (data) => (stderrData += data.toString())
+            (data) => (stderrData += data.toString()),
             console.log(stderrData)
         );
         ytDlpProcess.on('error', (error) => (processError = error));
